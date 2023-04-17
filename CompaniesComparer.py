0f791969fc6f5ee7a3067079175922b370c6aa3a
Markus Hofmann <markus.hofmann@gmail.com>
2023-04-17 21:12:07 +0200

    * Refactored compare method * Added TODO when HDAX has not 100 entries

--- CompaniesComparer.py
+++ CompaniesComparer.py
@@ -19,9 +19,9 @@ class CompaniesComparer:
         day = date.today()
         url = URL_TEMPLATE.format(day)
         print(url)
-        response = requests.get(url)
+        response = requests.get(url, verify=False)
         if response.status_code == 404:
-            # Be prepared that maybe during the week the excel sheet does not exist, use the last Friday
+            # Be prepared that maybe during the week the Excel sheet does not exist, use the last Friday
             print('Excel sheet not found. Retry!')
             if day.weekday() != 4:
                 day = date.today() + relativedelta(weekday=FR(-1))
@@ -29,13 +29,18 @@ class CompaniesComparer:
                 day = date.today() + relativedelta(days=-1)
             url = URL_TEMPLATE.format(day)
             print(url)
-            response = requests.get(url)
+            response = requests.get(url, verify=False)
 
         workbook = xlrd.open_workbook(file_contents=response.content)
         worksheet = workbook.sheet_by_index(1)
         return worksheet.col_values(5, 106)
 
     def compare(self, companies):
+        self.find_new_companies_inside_hdax(companies)
+        self.find_removed_companies_from_hdax(companies)
+        print('HDAX is up-to-date')
+
+    def find_new_companies_inside_hdax(self, companies):
         current_isins = self.international_securities_identification_numbers.copy()
         for company in companies:
             for isin in current_isins:
@@ -49,20 +54,21 @@ class CompaniesComparer:
             print("HDAX has changed!")
             print('New companies inside the HDAX')
             print(current_isins)
-
-            removed_companies = companies
-            for isin in self.international_securities_identification_numbers:
-                for company in removed_companies:
-                    if company["isin"] == isin:
-                        removed_companies.remove(company)
-                        continue
-
-            print('Removed companies from the HDAX')
-            removed = list()
-            for removed_company in removed_companies:
-                removed.append(removed_company['isin'])
-            print(removed)
-
             raise CompanyMismatchException()
 
-        print('HDAX is up-to-date')
+    def find_removed_companies_from_hdax(self, companies):
+        # TODO: It does not work when only 99 companies in the index
+        removed_companies = companies.copy()
+        for isin in self.international_securities_identification_numbers:
+            for company in removed_companies:
+                if company["isin"] == isin:
+                    removed_companies.remove(company)
+                    continue
+
+        print('Removed companies from the HDAX')
+        removed = list()
+        for removed_company in removed_companies:
+            removed.append(removed_company['isin'])
+        print(removed)
+       # raise CompanyMismatchException()
+
