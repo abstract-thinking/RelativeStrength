8cb1a473836570f78cf53440c63835874aa93b85
Markus Hofmann <markus.hofmann@gmail.com>
2023-04-17 21:12:53 +0200

    * Fixed Karfriday issue

--- ExcelWriter.py
+++ ExcelWriter.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
-from datetime import date, datetime
+from datetime import date
 from dateutil.relativedelta import relativedelta
+# https://towardsdatascience.com/the-easiest-way-to-identify-holidays-in-python-58333176af4f
+from workalendar.europe import Germany
 
 DATE_FORMAT = '%d.%m.%Y'
 
@@ -19,45 +21,50 @@ class ExcelWriter:
 
     def __init__(self, daily):
         self.daily = daily
-
-        self.fridays = self.find_fridays()
+        self.last_stock_exchange_days_of_the_week = ExcelWriter.find_last_stock_exchange_days_of_the_week()
+        print(self.last_stock_exchange_days_of_the_week)
         self.weekly = self.to_weekly()
 
     @staticmethod
-    def find_fridays():
+    def find_last_stock_exchange_days_of_the_week():
         last_quarter = date.today() - relativedelta(months=3)
-        return pd.date_range(start=str(last_quarter), end=str(date.today()), freq='W-FRI').strftime(
-            DATE_FORMAT).tolist()
+        last_fridays_of_the_week = pd.date_range(start=str(last_quarter), end=str(date.today()), freq='W-FRI')
+
+        last_stock_exchange_days_of_the_week = list()
+        for last_friday_of_the_week in last_fridays_of_the_week:
+            if Germany().is_working_day(ExcelWriter.to_date(last_friday_of_the_week)):
+                last_stock_exchange_days_of_the_week.append(last_friday_of_the_week.strftime(DATE_FORMAT))
+            else:
+                print("Found public holiday: " + last_friday_of_the_week.strftime(DATE_FORMAT))
+                prior_stock_exchange_day_of_the_week = ExcelWriter.find_prior_stock_exchange_day(last_friday_of_the_week)
+                last_stock_exchange_days_of_the_week.append(prior_stock_exchange_day_of_the_week.strftime(DATE_FORMAT))
+
+        return last_stock_exchange_days_of_the_week
+
+    @staticmethod
+    def find_prior_stock_exchange_day(stock_exchange_day):
+        de_calendar = Germany()
+        for prior_day in range(1, 5):
+            prior_stock_exchange_day = stock_exchange_day - pd.DateOffset(prior_day)
+            if de_calendar.is_working_day(ExcelWriter.to_date(prior_stock_exchange_day)):
+                print("Change stock exchange day to: " + prior_stock_exchange_day.strftime(DATE_FORMAT))
+                return stock_exchange_day
+
+        raise ValueError("Oops, no working day found")
+
+    @staticmethod
+    def to_date(exchange_day):
+        return date(exchange_day.year, exchange_day.month, exchange_day.day)
 
     def to_weekly(self):
-        dates = list()
-        for friday in self.fridays:
-            if friday in self.daily.columns:
-                # print(friday)
-                dates.append(friday)
-            else:
-                friday_not_found = datetime.strptime(friday, DATE_FORMAT)
-
-                previous_day = friday_not_found - relativedelta(days=1)
-                end_date = friday_not_found - relativedelta(days=3)
-
-                while previous_day >= end_date:
-                    this_day = previous_day.strftime(DATE_FORMAT)
-                    if this_day in self.daily.columns:
-                        print("Use a previous day: " + this_day)
-                        dates.append(this_day)
-                        break
-
-                    previous_day -= relativedelta(days=1)
-
-        tmp_weekly = self.daily.filter(dates)
+        tmp_weekly = self.daily.filter(self.last_stock_exchange_days_of_the_week)
         return tmp_weekly[tmp_weekly.columns[::-1]]
 
     def write_weekly(self):
         writer = pd.ExcelWriter("hdax.xlsx", engine='xlsxwriter')
 
         # write data to excel
-        self.weekly.sort_values(self.fridays[-1], ascending=False) \
+        self.weekly.sort_values(self.last_stock_exchange_days_of_the_week[-1], ascending=False) \
             .style \
             .apply(highlight_max) \
             .applymap(format_digits) \
